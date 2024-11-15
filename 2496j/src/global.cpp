0ee886c762068676dc31e4bb4922843d33e0ea73
Ayush Patel <120493345+Nameless2009@users.noreply.github.com>
2024-11-14 19:30:30 -0800

    added new devices for auton and driver control function

--- 2496j/src/global.cpp
+++ 2496j/src/global.cpp
@@ -8,24 +8,30 @@ namespace glb {
     Motor FL(P_FL_chassis, E_MOTOR_GEARSET_18, true);
     Motor BR(P_BR_chassis, E_MOTOR_GEARSET_18, false);
     Motor BL(P_BL_chassis, E_MOTOR_GEARSET_18, true);
-    Motor RM(P_RM_chassis, E_MOTOR_GEAR_600, false);
-    Motor LM(P_LM_chassis, E_MOTOR_GEAR_600, true);
+    Motor RM(P_MR_chassis, E_MOTOR_GEAR_600, false);
+    Motor LM(P_ML_chassis, E_MOTOR_GEAR_600, true);
 
 
-    Motor_Group rightChassis({P_FR_chassis, P_BR_chassis, P_RM_chassis});
-    Motor_Group leftChassis({P_FL_chassis, P_BL_chassis, P_LM_chassis});
-    Motor_Group chassis({P_FR_chassis, P_BR_chassis, P_FL_chassis, P_BL_chassis, P_RM_chassis, P_LM_chassis}); 
+    Motor_Group rightChassis({P_FR_chassis, P_BR_chassis, P_MR_chassis});
+    Motor_Group leftChassis({P_FL_chassis, P_BL_chassis, P_ML_chassis});
+    Motor_Group chassis({P_FR_chassis, P_BR_chassis, P_FL_chassis, P_BL_chassis, P_ML_chassis, P_MR_chassis});
 
-    Motor top_intake(P_top_intake, E_MOTOR_GEAR_600, true);
-    Motor lower_intake(P_lower_intake, E_MOTOR_GEAR_600, true);
+    Motor intake(P_intake, E_MOTOR_GEAR_600, true);
 
-    Motor lift(P_lift, E_MOTOR_GEAR_200, false);
+    Motor ladyBrownRight(P_ladyBrownRight, E_MOTOR_GEAR_200, false);
+    Motor ladyBrownLeft(P_ladyBrownLeft, E_MOTOR_GEAR_200, true);
+
+    Motor_Group ladyBrown({P_ladyBrownLeft, P_ladyBrownRight});
 
     ADIDigitalOut mogo('A', false);
+
+    ADIDigitalOut doinker('B', false);
 
     Imu inertial(P_inertial);
 
     Controller con(E_CONTROLLER_MASTER); // controller
 
+    Rotation lbrotation(P_rotation);
 
+    Distance clampDistance(P_distance);
 }
