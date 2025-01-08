da2aa16fd62e14e23ff44f171d199e3a43ff7e41
NamelessOne <120493345+Nameless2009@users.noreply.github.com>
2025-01-07 16:30:06 -0800

    5 ring so far

--- 2496j/src/autons.cpp
+++ 2496j/src/autons.cpp
@@ -1396,15 +1396,15 @@ void skipAutonomous()
 	mogo.set_value(true);
 	intake.move(127);
 	delay(100);
-	turnPIDMogo(-115);
+	turnPIDMogo(115);
 	drivePIDMogo(1100);
 	delay(150);
-	turnPIDMogo(-75);
+	turnPIDMogo(75);
 	drivePIDMogo(500);
 	delay(100);
 	drivePIDMogo(-1000);
-	turnPIDMogo(-45);
+	turnPIDMogo(40);
 	drivePIDMogo(800);
-	turnPIDMogo(-2);
-	//drivePIDMogo(2400);
+	turnPIDMogo(-5);
+	drivePIDMogo(2400);
 }
