3d4eb009b5dd30c09ae675f9998b12cc082b2bac
NamelessOne <120493345+vurse007@users.noreply.github.com>
2025-01-24 08:36:01 -0800

    stall protection fix
    stall protection task start is commented to prevent it from messing things up if the delays need to be tuned

--- 2496j/src/main.cpp
+++ 2496j/src/main.cpp
@@ -61,25 +61,24 @@ double stallTime;
 double reverseTime;
 
 void stallProtection(){
-
-	if (intake.get_actual_velocity() < 40.00 && stallLog == false){
-		stallTime = pros::millis();
-		stallLog = true;
-	}
-	if ((pros::millis() - stallTime) >= 500 && stallLog == true){
-		stallInterrupt = true;
-	}
-	if (stallInterrupt == true){
-		intake.move(-127);
-		reverseTime = pros::millis();
-		if ((pros::millis() - reverseTime) >= 500){
-			intake.move(127);
-			stallInterrupt = false;
-			stallLog = false;
+	while (1){
+		if (intake.get_actual_velocity() < 40.00 && stallLog == false){
+			stallTime = pros::millis();
+			stallLog = true;
+		}
+		if ((pros::millis() - stallTime) >= 500 && stallLog == true){
+			stallInterrupt = true;
+		}
+		if (stallInterrupt == true){
+			intake.move(-127);
+			reverseTime = pros::millis();
+			if ((pros::millis() - reverseTime) >= 500){
+				intake.move(127);
+				stallInterrupt = false;
+				stallLog = false;
+			}
 		}
 	}
-
-
 }
 
 void senseColor(){
@@ -507,7 +506,7 @@ void initialize()
 
 	pros::Task lbTask(ladyBrownTask);
 
-	pros::Task stallProtectionTask(stallProtection);
+	//pros::Task stallProtectionTask(stallProtection);
 }
 
 /**
